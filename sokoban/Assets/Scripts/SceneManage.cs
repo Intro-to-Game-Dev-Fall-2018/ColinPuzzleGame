a2c10428923ae070cfe2573e1d8875205161549f
LAPTOP-1L0MPP9Q\demok <ch3067@nyu.edu>
2018-10-22 10:29:36 -0400

    Sokoban Commit

---
+++ sokoban/Assets/Scripts/SceneManage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+	public int sceneInt;
+	public int Score;
+	public int ActiveScene;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Score = 0;
+		sceneInt = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			SceneManager.LoadScene("scene1");
+		}
+		if ((sceneInt == 1) && (Score == 3))
+		{
+			SceneManager.LoadScene("scene2");
+			sceneInt = 2;
+		}
+		
+	}
+}
